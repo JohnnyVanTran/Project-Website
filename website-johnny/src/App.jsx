02ebee7f243141e0b5cdb01a2dea392b8e1b08cc
Johnny  Tran <johnnytran@Johnnys-MacBook-Pro.local>
2025-09-07 13:39:31 +0200

    Bottombar

--- website-johnny/src/App.jsx
+++ website-johnny/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Intro from './components/Intro'
 import About from './components/About'
 import Skills from './components/Skills'
+import Bottom from './components/Bottom'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Intro />
       <About />
       <Skills />
+      <Bottom />
     </div>
   )
 }
---
+++ website-johnny/src/components/Bottom.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../component-styling/Bottom.css'
+
+
+function Bottom() {
+    return (
+        <div className="Ending">
+            <div className="Ending-text">
+                This page is to document my journey as I delve deeper into the abyss of Informatics
+            </div>
+        </div>
+    )
+}
+
+export default Bottom
